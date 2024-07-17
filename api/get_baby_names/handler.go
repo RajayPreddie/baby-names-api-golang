7f49185d8791ca418e40f190cbf82098208b4f95
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 10:50:06 -0400

    test

--- api/get_baby_names/handler.go
+++ api/get_baby_names/handler.go
@@ -1,20 +1,17 @@
 package get_baby_names
 
 import (
-	"babyname-api/database"
-	"babyname-api/models"
-	"encoding/json"
-
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var babyNames []models.BabyName
-	result := database.DB.Find(&babyNames)
-	if result.Error != nil {
-		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(babyNames)
+	/*
+		var babyNames []models.BabyName
+		result := database.DB.Find(&babyNames)
+		if result.Error != nil {
+			http.Error(w, result.Error.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(babyNames) */
 }
