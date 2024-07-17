f6cbacbc49ec9d7ff4c91181b487ca082d556776
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 02:43:02 -0400

    remove stuff from a handler

--- api/home/handler.go
+++ api/home/handler.go
@@ -4,31 +4,14 @@ import (
 	"babyname-api/database"
 	"babyname-api/models"
 	"encoding/json"
-	"log"
-	"os"
 
 	"net/http"
-
-	"github.com/joho/godotenv"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	var babyNames []models.BabyName
-	// Conditionally load .env file only during local development
-	if os.Getenv("VERCEL_ENV") == "" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file")
-		}
-	}
 
-	// Initialize configurations
-	database.Connect()
-	// Check if the database is connected
-	if database.DB == nil {
-		log.Fatalf("Database connection is nil")
-	}
 	result := database.DB.Find(&babyNames)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
