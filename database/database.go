aeef2bdc6d030e84acc78fe3cca72eda7d24d895
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 02:51:15 -0400

    connect only once

--- database/database.go
+++ database/database.go
@@ -3,23 +3,30 @@ package database
 import (
 	"log"
 	"os"
+	"sync"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 )
 
-var DB *gorm.DB
+var (
+	DB   *gorm.DB
+	once sync.Once
+)
 
+// Connect establishes a database connection if not already connected
 func Connect() {
-	dsn := os.Getenv("DB_DSN")
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
-	})
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
+	once.Do(func() {
+		dsn := os.Getenv("DB_DSN")
+		var err error
+		DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Silent),
+		})
+		if err != nil {
+			log.Fatalf("Failed to connect to database: %v", err)
+		}
 
-	log.Println("Connected to the database successfully.")
+		log.Println("Connected to the database successfully.")
+	})
 }
