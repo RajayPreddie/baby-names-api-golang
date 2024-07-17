4f3148f106cc27a0bc75574ec5fbd1bbd9679c97
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 03:07:04 -0400

    improve logging in a handler

--- api/home/handler.go
+++ api/home/handler.go
@@ -4,18 +4,29 @@ import (
 	"babyname-api/database"
 	"babyname-api/models"
 	"encoding/json"
+	"log"
 
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	// Check if the database is connected
+	if database.DB == nil {
+		log.Println("Database connection is nil")
+		http.Error(w, "Database connection error", http.StatusInternalServerError)
+		return
+	}
 
 	var babyNames []models.BabyName
 	result := database.DB.Find(&babyNames)
 	if result.Error != nil {
+		log.Printf("Error retrieving baby names: %v", result.Error)
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(babyNames)
+	if err := json.NewEncoder(w).Encode(babyNames); err != nil {
+		log.Printf("Error encoding response: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
