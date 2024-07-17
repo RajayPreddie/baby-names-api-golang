646506a2a0243382d57513c94854ac7f05ec09ac
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 02:58:39 -0400

    remove initialization for database and env from handler

--- api/home/handler.go
+++ api/home/handler.go
@@ -4,29 +4,13 @@ import (
 	"babyname-api/database"
 	"babyname-api/models"
 	"encoding/json"
-	"log"
-	"os"
 
 	"net/http"
-
-	"github.com/joho/godotenv"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	var babyNames []models.BabyName
-	// Conditionally load .env file only during local development
-	if os.Getenv("VERCEL_ENV") == "" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file")
-		}
-	}
-
-	// Check if the database is connected
-	if database.DB == nil {
-		database.Connect()
-	}
 	result := database.DB.Find(&babyNames)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
