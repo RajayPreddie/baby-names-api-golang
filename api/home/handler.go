329bc8fa3f491a5e76b9ccef098a2769fbf0f60d
Rajay Preddie <rajay.l.preddie@vanderbilt.edu>
2024-07-17 02:30:45 -0400

    add log

--- api/home/handler.go
+++ api/home/handler.go
@@ -4,6 +4,7 @@ import (
 	"babyname-api/database"
 	"babyname-api/models"
 	"encoding/json"
+	"log"
 
 	"net/http"
 )
@@ -11,6 +12,10 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	var babyNames []models.BabyName
+	// Check if the database is connected
+	if database.DB == nil {
+		log.Fatalf("Database connection is nil")
+	}
 	result := database.DB.Find(&babyNames)
 	if result.Error != nil {
 		http.Error(w, result.Error.Error(), http.StatusInternalServerError)
